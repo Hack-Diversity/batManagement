2ec57ee4774b426a7f1f3185b66a240868e79d84
Zainab Jalloh <zeejall@Zainabs-MacBook-Air.local>
2021-04-23 13:26:35 -0500

    Project Template - to be edited

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import logo from './assets/logo.svg';
+import bat from './assets/bat.svg';
+
+import './styles/App.css';
+
+import Dashboard from './components/dashboard/Dashboard';
+import Preferences from './components/preferences/Preferences';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={bat} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bat Management
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://id.atlassian.com/login?continue=https%3A%2F%2Fteam-1612214064928.atlassian.net%2Flogin%3FredirectCount%3D1%26dest-url%3D%252Fprojects%252FBMP%26application%3Djira&application=jira"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Coming Soon!
         </a>
       </header>
     </div>
